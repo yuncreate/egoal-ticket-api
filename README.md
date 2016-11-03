f49cd524f689756b313225526a57244f9f055829
mengkeys <mengkeys@hotmail.com>
2016-11-03 13:59:44 +0800

    更新注释，更新示例，完善校验

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ var ticket = new egoal(config);
 ## 获取产品接口
 
 ```js 
-ticket.getProductId(function(err, result){
+ticket.orderGetPdcId(function(err, result){
     console.log(err || result);
 });
 ```
@@ -35,15 +35,15 @@ ticket.getProductId(function(err, result){
 ## 门票购买接口
 
 ```js
-ticket.booking({
+ticket.orderCreate({
     threeOrderId: '你的订单编号',
     playDate:     '你的游玩日期',
     mobile:       '手机号',
     pdcId:        '产品1编号|产品2编号|产品3编号',
-    pdcPrice:     '产品1价格|产品2价格|产品3价格',
+    pdcPrice:     '产品1价格|产品2价格|产品3价格',                  //以“分”为单位的价格
     pCount:       '产品1数量|产品2数量|产品3数量',
     certNo:       '客户1身份证号码|客户2身份证号码|客户3身份证号码',
-    money:        '订单总金额，分为单位'
+    money:        '订单总金额'                                    //以“分”为单位的总价  
 }, function(err, result){
     console.log(err || result);
 });
@@ -52,11 +52,11 @@ ticket.booking({
 ## 订单状态接口
 
 ```js
-ticket.getBookingStatus('你自己系统的订单编号', function(err, result){
+ticket.orderGetInfo('你自己系统的订单编号', function(err, result){
     console.log(err || result);
 });
 
-ticket.getBookingStatus({threeOrderId:"你自己系统的订单编号"}, function(err, result){
+ticket.orderGetInfo({threeOrderId:"你自己系统的订单编号"}, function(err, result){
     console.log(err || result);
 });
 ```
@@ -64,7 +64,7 @@ ticket.getBookingStatus({threeOrderId:"你自己系统的订单编号"}, functio
 ## 订单退订接口
 
 ```js
-ticket.cancelBooking({
+ticket.orderCancel({
     orderId:        "易高系统订单编号",
     threeOrderId:   "你自己系统的订单编号",
     ticketCode:     "票号"
@@ -76,7 +76,7 @@ ticket.cancelBooking({
 ## 消费通知接口
 
 ```js
-ticket.notify(req, res, function(result){
+ticket.ticketNotice(req, res, function(result){
     console.log(result);
     return res.json({status:1, message:'receive success.'});
 });
@@ -84,9 +84,7 @@ ticket.notify(req, res, function(result){
 
 
 ## 相关链接
-
+##### [易高官方网站](www.egoal.com.cn)
 ##### [易高官方文档](https://github.com/yuncreate/egoal-ticket-api/blob/master/%E6%98%93%E9%AB%98%E7%BD%91%E4%B8%8A%E8%B4%AD%E7%A5%A8%E7%B3%BB%E7%BB%9F%E6%8E%A5%E5%8F%A3%E8%AF%B4%E6%98%8E(V2.1).pdf)（官方V2.1PDF手册）
 ##### [Issues列表](https://github.com/yuncreate/egoal-ticket-api/issues)
-
-
-##### 版权声明：本模块由[丽江云创信息技术有限公司](http://www.yuncreate.com)提供，并由[Mengkeys](http://www.mengkeys.com)负责维护。
+##### [版权声明]：本模块由[丽江云创信息技术有限公司](http://www.yuncreate.com)提供，并由[Mengkeys](http://www.mengkeys.com)负责维护。
--- egoal.js
+++ egoal.js
@@ -1,18 +1,13 @@
 /**
- * Created by mengkeys on 2016-10-31.
- */
-
-
-/**
- * Copyright(c) yuncreate
- * Email: <yuncreate@163.com> (https://www.yuncreate.con)
+ * Copyright(c) 丽江云创信息技术有限公司
  *
- * MIT Licensed
+ * Authors: mengkeys <mengkeys@hotmail.com>
  *
- * Authors:
- *   mengkeys <mengkeys@hotmail.com>
+ * Website: https://www.yuncreate.con
  *
- *   易高售票系统API接口调用工具
+ * Version: 2.0
+ *
+ * License: MIT Licensed
  */
 
 var md5    = require('md5');
@@ -20,39 +15,56 @@ var qs     = require('querystring');
 var request = require('request');
 var validator = require('validator');
 
-/*
- * args options sort (参数对象排序)
- * 参数排序
- * 英文升序排列
- * 返回排序后的对象
+/**
+ * 对象属性排序
+ * @name   objectSort
+ * @param  {Object} obj    (待处理对象)
+ * @return {Object} result (处理后的对象)
  */
 
-function objectSort(Obj){
+function objectSort(obj){
     var result = {};
-    var items = Object.keys(Obj).sort();
+    var items = Object.keys(obj).sort();
     for(var item in items){
-        result[items[item]] = Obj[items[item]];
+        result[items[item]] = obj[items[item]];
     }
     return result;
 }
 
-/* Create Object　*/
+
+/**
+ * 去空操作
+ * @name trim
+ * @param  {String} str (待操作字符串)
+ * @return {String} str (处理结果)
+ */
+function trim(str) {
+      return str.replace(/(^\s*)|(\s*$)/g, ""); 
+}
+
+/**
+ * 构造对象
+ * @name   Ticket
+ * @param  {Object} opt    ({url, uname, pass})
+ * @return {Object} object (可操作对象)
+ *
+ */
 function Ticket(opt){
     if(arguments.length < 1 || typeof opt !== 'object'){
         throw new Error('options required');
     }
 
     // verify uname（检测账号）
-    if(!opt.hasOwnProperty('uname')){
+    if(!opt.hasOwnProperty('uname') || trim(opt.uname) == ""){
         throw new Error('uname required.');
     }
 
     // verify pass（检测密码）
-    if(!opt.hasOwnProperty('pass')){
+    if(!opt.hasOwnProperty('pass') || trim(opt.pass) == ""){
         throw new Error('pass required.');
     }
 
-    if(!opt.hasOwnProperty('url')){
+    if(!opt.hasOwnProperty('url') || trim(opt.url) == ""){
         throw new Error('api url required.');
     }
 
@@ -62,37 +74,72 @@ function Ticket(opt){
     this.url = opt.url;  // api remote address.
 }
 
-/*
- * 接口请求参数处理
- * @param <Object> opt (请求参数对象)
- * @return <String> (签名)
+/**
+ * 签名处理
+ * @name   signature
+ * @param  {Object} opt  (请求参数对象)
+ * @return {String} sign (签名)
  */
 
+
 Ticket.prototype.signature = function(opt){
-    console.log(objectSort(opt));
     return md5((md5(qs.stringify(objectSort(opt)))+this.pass)).toUpperCase();
 };
 
-/*
+/**
  * 发起接口调用
+ * @name  request
+ * @param {Object}   opt
+ * @param {Function} callback
  */
 
 Ticket.prototype.request = function(opt, callback){
-    console.log(this.url);
-    console.log(opt);
     request.post(this.url, {form:opt}, function (err, res, body) {
         if(err) return callback(err);
         return callback(null, JSON.parse(body));
     });
 };
 
-/*
- *  获取产品ID接口
- *  @param callback [Function]
+/**
+ * 门票列表接口
+ * Examples:
+ * ```
+ * ticket.orderCreate(callback);
+ * ```
+ * Callback:
+ *
+ * - `err`, 调用失败时得到的异常
+ * - `result`, 调用正常时得到的对象
+ *
+ * Result:
+ * Fail:
+ * ```
+ * {
+ *   code:"0",
+ *   message:"错误信息"
+ * }
+ *
+ * ```
+ * Success:
+ * ```
+ * {
+ *   status: '1',
+ *   message: '查询成功',
+ *   pdcIds: [{
+ *      pdcId: '1',
+ *      pdcName: '成人票',
+ *      pdcPrice: '1',
+ *      days: '20'
+ *   }]
+ * }
+ * ```
+  *
+ * @name  orderGetPdcId
+ * @param {Function} callback (回调函数)
  */
-Ticket.prototype.getProductId = function (callback) {
+Ticket.prototype.orderGetPdcId = function (callback) {
     if(arguments.length < 1 || typeof callback != 'function'){
-        throw new Error('please provide callback function');
+        throw new Error('callback function required.');
     }
     var opt = {
         method: 'order_getpdcId',
@@ -102,13 +149,54 @@ Ticket.prototype.getProductId = function (callback) {
     this.request(opt, callback);
 };
 
+/**
+ * 购买接口
+ * Example
+ * ```
+ * ticket.orderCreate({
+ *   threeOrderId: '第三方系统订单编号',
+ *   playDate:     '2016-11-08',
+ *   mobile:       '手机号',
+ *   pdcId:        '1',
+ *   pdcPrice:     '100',
+ *   checkNum:     '1',
+ *   pCount:       '1',
+ *   certNo:       '客户身份证号',
+ *   money:        '100',
+ *   localPay:     '0'
+ * }, callback);
+ * ```
+ * Callback:
+ *
+ * - `err`, 调用失败时得到的异常
+ * - `result`, 调用正常时得到的对象
+ *
+ * Result:
+ * Fail:
+ * ```
+ * {
+ *   code:"0",
+ *   message:"错误信息"
+ * }
+ *
+ * ```
+ * Success:
+ * ```
+ *{"status":"0",
+ *   "orderId":"T15090600000001",
+ *   "message":"查询成功",
+ *   "ticketCodes":[
+ *       {"pdcId":"1","ticketCode":"010000016813"},
+ *       {"pdcId":"2","ticketCode":"010000016814"}]
+ *}
+ * ```
 
-/*
- *  下单接口
- *  @param opt      [Object]
- *  @param callback [Function]
+
+ *  @name  orderCreate          (购票接口)
+ *  @param opt      {Object}    (订单数据对象)
+ *  @param callback {Function}  (回调函数)
  */
-Ticket.prototype.booking = function(opt, callback){
+Ticket.prototype.orderCreate = function(opt, callback){
     if(arguments.length < 1){
         throw  new Error('please provide options and callback');
     }
@@ -122,42 +210,41 @@ Ticket.prototype.booking = function(opt, callback){
         return opt(new Error('booking options object required.'));
     }
 
-    if(!opt.hasOwnProperty('threeOrderId')){
+    if(!opt.hasOwnProperty('threeOrderId') || trim(opt.threeOrderId) == ""){
         return callback(new Error('options.threeOrderId required.'));
     }
 
-    if(!opt.hasOwnProperty('playDate')){
+    if(!opt.hasOwnProperty('playDate') || trim(opt.playDate) == ""){
         return callback(new Error('options.playDate required.'));
     }
 
-    if(!opt.hasOwnProperty('mobile')){
+    if(!opt.hasOwnProperty('mobile') || trim(opt.mobile) == ""){
         return callback(new Error('options.mobile required.'));
     }
 
-    if(!opt.hasOwnProperty('pdcId')){
+    if(!opt.hasOwnProperty('pdcId') || trim(opt.pdcId) == ""){
         return callback(new Error('options.pdcId required.'));
     }
 
-    if(!opt.hasOwnProperty('pdcPrice')){
+    if(!opt.hasOwnProperty('pdcPrice') || trim(opt.pdcPrice) == ""){
         return callback(new Error('options.pdcPrice required.'));
     }
 
-    if(!opt.hasOwnProperty('pCount')){
+    if(!opt.hasOwnProperty('pCount') || trim(opt.pCount) == ""){
         return callback(new Error('options.pCount required.'));
     }
 
-    if(!opt.hasOwnProperty('checkNum')){
+    if(!opt.hasOwnProperty('checkNum') || trim(opt.checkNum) == ""){
         return callback(new Error('options.checkNum required.'));
     }
 
-    if(!opt.hasOwnProperty('money')){
+    if(!opt.hasOwnProperty('money') || trim(opt.money) == ""){
         return callback(new Error('options.money required.'));
     }
 
-    if(!opt.hasOwnProperty('localPay')){
+    if(!opt.hasOwnProperty('localPay') || trim(opt.localPay) == ""){
         return callback(new Error('options.localPay required.'));
     }
-
 
     var threeOrderId    = opt.threeOrderId,
         playDate        = opt.playDate,
@@ -178,9 +265,11 @@ Ticket.prototype.booking = function(opt, callback){
         return callback(new Error('mobile invalid.'));
     }
     // pdcId, pdcPrice, pCount 联合校验
-    if(pdcId.split('|').length != pdcPrice.split('|').length != pCount.split('|')){
+    if(pdcId.split('|').length != pdcPrice.split('|').length || pdcId.split('|').length != pCount.split('|').length || pdcPrice.split('|').length != pCount.split('|').length){
         return callback(new Error('pdcId, pdcPrice, pCount not match'));
     }
+
+    // 匹配问题
 
     var args = {
         method: 'order_create',
@@ -197,26 +286,57 @@ Ticket.prototype.booking = function(opt, callback){
     this.request(args, callback);
 };
 
-/*
- *  订单退订接口
- *  第三方平台通知本票务系统退票
+/**
+ *  退票接口
+ *  Example
+ *  ```
+ *  ticket.orderCancel({
+ *      threeOrderId: '第三方系统订单编号',
+ *      orderId:      '易高系统订单编号',
+ *      ticketCode:   '票号'
+ *  }, function(err, result){
+ *      console.log(err || result);
+ *  })
+ *  ```
+ *  Callback:
+ *
+ * - `err`, 调用失败时得到的异常
+ * - `result`, 调用正常时得到的对象
+ *
+ * Result:
+ * Fail:
+ * ```
+ * {
+ *   code:"0",
+ *   message:"错误信息"
+ * }
+ * ```
+ * Success:
+ * ```
+ * {
+ *   code:"0",
+ *   message:"申请退票成功"
+ * }
+ * ```
+ *  @name  orderCancel         (订单退订接口)
+ *  @param {Object}   opt      (参数对象)    {Object{orderId:"", threeOrderId:"",ticketCode:""}}
+ *  @param {Function} callback (回调函数)
  */
-
-Ticket.prototype.cancelBooking = function (opt, callback) {
+Ticket.prototype.orderCancel = function (opt, callback) {
     if(arguments.length < 1 || typeof opt !== 'object'){
         // 错误
         throw new Error('Params Invalid.');
     }
 
-    if(!opt.hasOwnProperty('orderId')){
+    if(!opt.hasOwnProperty('orderId') || trim(opt.orderId) == ""){
         return callback(new Error('orderId required'));
     }
 
-    if(!opt.hasOwnProperty('threeOrderId')){
+    if(!opt.hasOwnProperty('threeOrderId') || trim(opt.threeOrderId) == ""){
         return callback(new Error('threeOrderId required'));
     }
 
-    if(!opt.hasOwnProperty('ticketCode')){
+    if(!opt.hasOwnProperty('ticketCode') || trim(opt.ticketCode) == ""){
         return callback(new Error('ticketCode required'));
     }
 
@@ -232,10 +352,47 @@ Ticket.prototype.cancelBooking = function (opt, callback) {
     this.request(args, callback);
 };
 
-/*
- *  获取订单状态
+/**
+ *  订单信息接口
+ *  Example
+ *  ```
+ *  ticket.orderCancel({
+ *      threeOrderId: '第三方系统订单编号',
+ *      orderId:      '易高系统订单编号',
+ *      ticketCode:   '票号'
+ *  }, function(err, result){
+ *      console.log(err || result);
+ *  })
+ *  ```
+ *  Callback:
+ *
+ * - `err`, 调用失败时得到的异常
+ * - `result`, 调用正常时得到的对象
+ *
+ * Result:
+ * Fail:
+ * ```
+ * {
+ *   code:"0",
+ *   message:"错误信息"
+ * }
+ * ```
+ * Success:
+ * ```
+ * {"status":"1",
+ *      "message":"订单详情",
+ *      "orderId":"T15090600000001",
+ *      "orderStatus ":[
+ *          {"pdcId":"1","ticketCode":"010000016813","orderStatus ":"1"},
+ *          {"pdcId":"2","ticketCode":"010000016814","orderStatus ": "1"}]
+ * }
+ * ```
+ *  @name  orderGetInfo              (获取订单状态)
+ *  @param {String}   threeOrderId   (第三方系统订单编号)
+ *  @param {Function} callback       (回调函数)
+ *
  */
-Ticket.prototype.getBookingStatus = function (threeOrderId, callback) {
+Ticket.prototype.orderGetInfo = function (threeOrderId, callback) {
     if(typeof threeOrderId == 'function'){
         //threeOrderId = threeOrderId.threeOrderId;
         return threeOrderId(new Error('threeOrderId param required.'));
@@ -245,7 +402,7 @@ Ticket.prototype.getBookingStatus = function (threeOrderId, callback) {
         threeOrderId = threeOrderId.threeOrderId;
     }
 
-    if(!threeOrderId){
+    if(!threeOrderId || trim(threeOrderId) == ""){
         return callback(new Error('threeOrderId param required.'));
     }
     // 逻辑校验
@@ -262,39 +419,76 @@ Ticket.prototype.getBookingStatus = function (threeOrderId, callback) {
     this.request(args, callback);
 };
 
-//
-/*
+/**
  *  消费通知接口
- *  @param req      [Object]
- *  @param res      [Object]
- *  @param callback [Function]
+ *  Example
+ *  ```
+ *  ticket.ticketNotice(req, res, function(err, result){
+ *      console.log(err || result);
+ *  });
+ *  ```
+ *  Callback:
+ *
+ * - `err`, 调用失败时得到的异常
+ * - `result`, 调用正常时得到的对象
+ *
+ * Result:
+ * Fail:
+ * ```
+ * {
+ *   code:"0",
+ *   message:"错误信息"
+ * }
+ * ```
+ * Success:
+ * ```
+ * {
+ *  "status":"1",
+ *  "message":"订单详情",
+ *  "sign":"",
+ *  "uname":"",
+ *  "orderId":"T15090600000001",
+ *  "threeOrderId":"",
+ *  "ticketCode":"010000016813",
+ * }
+ * ```
+ *  @name  ticketNotice          (消费通知接口)
+ *  @param req      {Object}     (request对象)
+ *  @param res      {Object}     (response对象)
+ *  @param callback {Function}   (回调函数)
  */
-Ticket.prototype.notify = function (req, res, callback) {
+Ticket.prototype.ticketNotice = function (req, res, callback) {
     var self = this;
     try{
         var data = req.body;
-        if(!data.hasOwnProperty('method')){
-            return res.json({status:0, message:'method param not provide.'});
+        if(!data.hasOwnProperty('method') || trim(data.method) == ""){
+            throw new Error('method param not provide.');
+            //return res.json({status:0, message:'method param not provide.'});
         }
 
-        if(!data.hasOwnProperty('sign')){
-            return res.json({status:0, message:'sign param not provide'});
+        if(!data.hasOwnProperty('sign') || trim(data.sign) == ""){
+            throw new Error('sign param not provide');
+            //return res.json({status:0, message:'sign param not provide'});
         }
 
-        if(!data.hasOwnProperty('uname')){
-            return res.json({status:0, message:'uname param not provide'});
+        if(!data.hasOwnProperty('uname') || trim(data.uname) == ""){
+            throw new Error('uname param not provide');
+            //return res.json({status:0, message:'uname param not provide'});
         }
 
-        if(!data.hasOwnProperty('orderId')){
-            return res.json({status:0, message:'orderId param not provide'});
+        if(!data.hasOwnProperty('orderId') || trim(data.orderId) == ""){
+            throw new Error('orderId param not provide');
+            //return res.json({status:0, message:'orderId param not provide'});
         }
 
-        if(!data.hasOwnProperty('threeOrderId')){
-            return res.json({status:0, message:'threeOrderId param not provide'})
+        if(!data.hasOwnProperty('threeOrderId') || trim(data.threeOrderId) == ""){
+            throw new Error('threeOrderId param not provide');
+            //return res.json({status:0, message:'threeOrderId param not provide'})
         }
 
-        if(!data.hasOwnProperty('ticketCode')){
-            return res.json({status:0, message:''})
+        if(!data.hasOwnProperty('ticketCode') || trim(data.ticketCode) == ""){
+            throw new Error('ticketCode param not provide.');
+            //return res.json({status:0, message:''})
         }
     } catch (e){
         return res.json({status:0,message:e.message});
